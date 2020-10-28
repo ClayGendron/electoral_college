8dbafa18020ddcc76645d74c0077e0dcc985d65f
Gendron <c.gendron1@snhu.edu>
2020-10-28 00:05:44 -0400

    updates

--- R/electoral_college.R
+++ R/electoral_college.R
@@ -8,9 +8,10 @@ library(corrplot)
 #data pull
 pe_2016 <- read.csv(here::here("Data/Presidential Election Data (2016).csv"))
 colnames(pe_2016)
+# ("State" "Year" "Democrat.Votes" "Democrat.EC", "Republican.Votes", "Republican.EC", "Third.Party.Votes", "Third.Party.EC", "Vote.Margin..ABS.", "Vote.Margin", "Vote.Margin....ABS.", "Vote.Margin..", "Total.Votes.Minus.Third.Party", "Total.Votes", "Total.EC", "Registered.Voters", "Voting.Aged.Population", "State.Population", "Electoral.Power.Group", "State.Population.Over.EC", "Turn.Out", "Rural.Population", "Small.Town.Population", "Suburb.Population", "Urban.Population", "White", "Black", "Hispanic", "Asian", "Two.or.More.Races.Other", "High.School", "Bachelors.Degree", "Advanced.Degree", "Children.0.18", "Adults.19.25", "Adults.26.34", "Adults.35.54", "Adults.55.64", "Adults.65.", "Below.Poverty....100..", "Above.Poverty..100...199..", "Above.Poverty..200.399..", "Abover.Poverty..400...", "Male", "Female")
 summary(pe_2016)
 
-pe_corr_df <- pe_2016 %>% select("VoteMarginPercentABS", "VoteMarginPercent", "TurnOut", "RuralPopulation","SmallTownPopulation", "SuburbPopulation", "UrbanPopulation","White", "Black", "Hispanic","AmericanIndian.AlaskaNative", "Asian", "NativeHawaiian.OtherPacificIslander", "TwoOrMoreRaces", "HighSchool", "BachelorsDegree","AdvancedDegree")
+pe_corr_df <- pe_2016 %>% select("Vote.Margin..", "Turn.Out", "Rural.Population", "Small.Town.Population", "Suburb.Population", "Urban.Population", "White", "Black", "Hispanic", "Asian", "Two.or.More.Races.Other","No.High.School", "High.School", "Bachelors.Degree", "Advanced.Degree", "Children.0.18", "Adults.19.25", "Adults.26.34", "Adults.35.54", "Adults.55.64", "Adults.65.", "Below.Poverty....100..", "Above.Poverty..100...199..", "Above.Poverty..200.399..", "Abover.Poverty..400...", "Male", "Female")
 
 
 white_m <- mean(pe_2016$White)
@@ -34,4 +35,5 @@ margin_lm <- lm(VoteMarginPercent ~
 summary(margin_lm)
 
 pe_corr <- cor(pe_corr_df)
+pe_corr
 corrplot(pe_corr)
